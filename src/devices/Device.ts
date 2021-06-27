4257e83b4b43bffcdb86c88fadbb343554ece399
aetheryx <zainalih3c@gmail.com>
2021-06-27 04:50:11 +0200

    why does stream export eventemitter

--- src/devices/Device.ts
+++ src/devices/Device.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from 'stream';
+import { EventEmitter } from 'events';
 import TuyaAPIDevice, { DataPointsPayload, DataPointValue } from 'tuyapi';
 import { DeviceOptions } from '../types';
 
